6787cf14fa93d0f3626813e6c809e32d0018a979
wanderdust <pablo.lopez.santori@gmail.com>
2019-08-25 14:09:15 +0100

    refactor: removed unused dependencies and update all packages

--- webpack.config.js
+++ webpack.config.js
@@ -67,15 +67,7 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      CSSExtract,
-      new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-      })
+      CSSExtract
     ],
     /*
     * Hashmap for debugging. Shows error in pre-compiled files rather than in bundle.js.
